cb40185295676ac2558a1d6c0f3e9ed3fe9bec09
Ashutosh Anshu <ashutosh.anshu@optum.com>
2021-06-02 12:07:15 +0530

    day1-end

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BiArchive } from "react-icons/bi"
 function App() {
   return (
     <div className="App">
-      <h1><BiArchive /></h1>
+      <h1><BiArchive />Your appointments</h1>
     </div>
   );
 }
 
 export default App;
+ 
